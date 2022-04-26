353c625d8103a6b6a92b3c47ed446b6ae5f7ceca
ArulMC123 <99807639+ArulMC123@users.noreply.github.com>
2022-04-26 16:03:26 +0700

    Update mess.js

--- message/mess.js
+++ message/mess.js
@@ -1,20 +1,20 @@
 module.exports.mess = {
-	        regist: '─ 「 *NOT REGISTERED* 」 ─\n\n*ENGLISH*\n_YOU HAVE NOT REGISTERED ON THE BOTTOM OF I DECK, TYPE .verify FOR RUQYAH_\n\n*INDONESIA*\n_LUH BELUM DAFTAR DI BOT GUA DEX, KETIK .daftar UNTUK DI RUQYAH_',
+	        regist: '─ 「 *NOT REGISTERED* 」 ─\n\n*ENGLISH*\n_YOU HAVE NOT REGISTERED ON THE BOTTOM OF I DECK, TYPE .verify FOR RUQYAH_\n\n*INDONESIA*\n_LUH BELUM DAFTAR DI BOT GUA DEX, KETIK .daftar UNTUK Daftar Member Bot_',
 		    dewasa: '─ 「 *WARNING* 」 ─\n\n*ENGLISH*\n_not allowed for minors, if you are an adult, please verify first, type .my18_\n\n*INDONESIA*\n_dilarang untuk anak di bawah umur, jika Anda sudah dewasa, silahkan verifikasi terlebih dahulu, ketik .saya18_',
 		    waitregist: '*ENGLISH*\n_WAIT A MINUTE DECK_\n\n*INDONESIA*\n_TUNGGU SEBENTAR DEK_',
 	        wait: '_Wait Process!..._',
-			success: '_Done, don t forget to subscribe_ : https://youtube.com/channel/UCB157jomCne961WzYHpG4gg',
+			success: '_Done, Silahkan Subscribe Channel Owner_ : https://youtube.com/channel/UCJh9I1GEVJk8qQWGs1t09eQ',
 			wrongFormat: '_Format error, try again!_',
 			error: {
-				api: '_Error, apikey is not found!_',
-				stick: '_Error, just try to send a picture!_',
-				Iv: '_Error, try to give the correct link!_'
+				api: '_Error 404, apikey Tidak Ada!_',
+				stick: '_Error 404, Itu Bukan png!_',
+				Iv: '_Error 404, Coba Berikan Link Yang Benar!_'
 			},
 			only: {
-				group: '_Can only be used in groups!_',
-				admin: '_Can only be used by admin!_',
-				premium: '_Can only be used by premium users!_',
-				owner: '_Can only be used by owner!_',
-				Badmin: '_Can only be used if the bot is an admin!_',
+				group: '_Fitur Hanya Untuk Group !!!_',
+				admin: '_Fitur Khusus Admin !!!_',
+				premium: '_Fitur Khusus Pengguna Premium !!!_',
+				owner: '_Fitur Khusus Owner !!!_',
+				Badmin: '_Bot Harus Menjadi Admin Terlebuh Dahulu !!!_',
 			}
 		}
